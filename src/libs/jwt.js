e8666b85dd7dd80d353b2dc359f04b3ed72214af
torcoroma <torcoromaarias@gmail.com>
2024-03-07 10:42:48 -0500

    erro solucionado

--- src/libs/jwt.js
+++ src/libs/jwt.js
@@ -1,12 +1,13 @@
+
+import jwt from "jsonwebtoken";
 import { TOKEN_SECRET } from "../config.js";
-import  jwt  from "jsonwebtoken";
 export const createdAccessToken = (payload) => {
   return new Promise((resolve, reject) => {
     jwt.sign(
       payload,
       TOKEN_SECRET,
       {
-        expiresIn: 1,
+        expiresIn: "id"
       },
       (err, token) => {
         if (err) reject(err);
--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -3,13 +3,21 @@ import { TOKEN_SECRET } from "../config.js";
 console.log(TOKEN_SECRET, "totototot");
 export const authQuired = (req, res, next) => {
   const { token } = req.cookies;
+  console.log(token);
+  if (!token) {
+    return res.status(401).json({ message: "Token no proporcionado" });
+  }
 
-  if (!token) return res.status(401).json({ message: "no token " });
-  console.log(token, "hola");
-  jwt.verify(token, TOKEN_SECRET, (err, user) => {
-    if (err) return res.status(403).json({ message: "token invalido" });
-      console.log(user)
-       next();
+  jwt.verify(token, TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).json({ message: "Token inválido" });
+    }
+
+    // El ID del usuario estará disponible en `decoded.id`
+    console.log("ID del usuario:", decoded);
+    req.decoded = decoded
+    //  console.log("ID del usuario:", decoded.id);
+
+    next();
   });
- 
 };

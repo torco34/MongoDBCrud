1686ee8fbb7a7428e9c580f9f22e0443f8f7b8d4
torcoroma <torcoromaarias@gmail.com>
2024-04-17 21:40:13 -0500

    front

--- frontendreact/src/pages/RegisterPages.jsx
+++ frontendreact/src/pages/RegisterPages.jsx
@@ -5,25 +5,25 @@ export const RegisterPages = () => {
   const onSubmit = (data) => console.log(data);
 
   return (
-    <div className="bg-zinc-800 max-w-md p-10 rounded-md">
+    <div className="bg-zinc-800 max-w-md p-10 rounded-md container">
       <form onSubmit={handleSubmit(onSubmit)}>
-        <input type="text"
-          {...register("username", {required: true})}
-          className="w-full bg-zinc-700 text-left px-4 py-2 rounded-md"
+        <input
+          type="text"
+          {...register("username", { required: true })}
+          className="w-full bg-zinc-700 text-left px-4 py-2 rounded-md my-2"
+          placeholder="Nombre"
+        />
+        <input
+          {...register("Email")}
+          className="w-full bg-zinc-700 text-left px-4 py-2 rounded-md my-2"
+          placeholder="Email"
         />
         <input
           {...register("firstName")}
-          className="w-full bg-zinc-700 text-left px-4 py-2 rounded-md"
+          className="w-full bg-zinc-700 text-left px-4 py-2 rounded-md my-2"
+          placeholder="password"
         />
-        <input
-          {...register("firstName")}
-          className="w-full bg-zinc-700 text-left px-4 py-2 rounded-md"
-        />
-        <input
-          {...register("firstName")}
-          className="w-full bg-zinc-700 text-left px-4 py-2 rounded-md"
-        />
-        <input type="submit" />
+        <input type="submit" className="my-4" />
       </form>
     </div>
   );
